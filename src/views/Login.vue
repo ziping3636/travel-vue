c200d5695d68a63fe01ab9e8f874fbc6ad2232e5
yangjinjing <2958275370@qq.com>
2020-05-19 21:41:13 +0800

    用户登录功能

---
+++ src/views/Login.vue
@@ -0,0 +1,116 @@
+<template>
+	<div class="card">
+		<div>
+			      <el-form ref="loginForm" :model="loginModel" :rules="rules" label-width="80px" class="login-box">
+			        <h3 class="login-title">XXX旅游账号登录</h3>
+					
+			        <el-form-item label="账号">
+			          <el-input type="text" placeholder="请输入账号" v-model="loginModel.username"/>
+			        </el-form-item>
+					
+			        <el-form-item label="密码">
+			          <el-input type="password" placeholder="请输入密码" v-model="loginModel.password"/>
+			        </el-form-item>
+					
+			        <el-form-item>
+			          <el-button type="primary" @click="onSubmit('loginForm')">登录</el-button>
+			        </el-form-item>
+					
+					
+			      </el-form>
+			  
+			      <el-dialog
+			        title="温馨提示"
+			        :visible.sync="dialogVisible"
+			        width="30%"
+			        >
+			        <span>请输入账号和密码</span>
+			        <span slot="footer" class="dialog-footer">
+			          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+			        </span>
+			      </el-dialog>
+			   </div>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios'
+  export default {
+    name: "Login",
+    data() {
+      return {
+        loginModel: {
+          username: '',
+          password: ''
+        },
+
+        // 表单验证，需要在 el-form-item 元素中增加 prop 属性
+        rules: {
+          username: [
+            {required: true, message: '账号不可为空', trigger: 'blur'},
+          ],
+          password: [
+            {required: true, message: '密码不可为空', trigger: 'blur'}
+          ]
+        },
+
+        // 对话框显示和隐藏
+        dialogVisible: false
+      }
+    },
+    methods: {
+      onSubmit() {
+				this.axios.get("http://localhost:92/user/login", {
+					params: {
+						username: this.loginModel.username,
+						password: this.loginModel.password
+					}
+				}).then((res) => {
+					console.log(res.data.msg =="ok");
+					if (res.data.msg == "ok") {
+						//mutations提交
+						//this.$store.commit('login', this.loginForm);
+						//action提交
+						this.$store.dispatch("loginAction", this.loginModel);
+						//转换路由操作
+						this.$router.push("home");
+					} else {
+						this.$message.error("登陆失败");
+					}
+				})
+			},
+    }
+  }
+</script>
+
+
+<style>
+	/*.homeBox {
+		position: fixed;
+		width: 100%;
+		height: 100%;
+		top: 0px;
+		background-color: #ffffff;
+	}*/
+  .login-box {
+	      border: 1px solid #DCDFE6;
+	      width: 350px;
+	      margin: 180px auto;
+	      padding: 35px 35px 15px 35px;
+	      border-radius: 5px;
+	      -webkit-border-radius: 5px;
+	      -moz-border-radius: 5px;
+	      box-shadow: 0 0 25px #909399;
+	      /*position: fixed;*/
+			/*width: 100%;
+			height: 100%;*/
+			top: 0px;
+			background-color: #ffffff;
+	    }
+	  
+	    .login-title {
+	      text-align: center;
+	      margin: 0 auto 40px auto;
+	      color: #303133;
+	    }
+</style>
